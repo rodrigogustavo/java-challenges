946758dcf83c53cb318ab8954f1d0a526ff1985a
RodrigoGustavo <rodrigogustavo@gmail.com>
2018-09-15 19:04:04 -0400

    fixing 30 days of code challenge 2.

--- src/main/java/com/rodrigo/java_challenges/hh/thirtydaysofcode/Operators.java
+++ src/main/java/com/rodrigo/java_challenges/hh/thirtydaysofcode/Operators.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Operators {
     // Complete the solve function below.
     static void solve(double meal_cost, int tip_percent, int tax_percent) {
-        double total = meal_cost + (meal_cost * tip_percent) / 100 + (meal_cost + tax_percent) / 100;
+        double total = meal_cost + (meal_cost * tip_percent) / 100 + (meal_cost * tax_percent) / 100;
         int totalInt = (int) Math.round(total);
         System.out.println("The total meal cost is " + totalInt + " dollars.");
 
